23e35f269874709255ab572af6f9ca72d3ac683f
Jérôme <jfreyheit@gmail.com>
2025-03-14 21:30:54 +0530

    feat: improve Kyseky queries : specify table name, better defaults, allow to edit whole path

--- src/helpers/kyselyQueriesPath.ts
+++ src/helpers/kyselyQueriesPath.ts
@@ -3,5 +3,5 @@ import { snakeCase } from "change-case";
 import { removeTrailingSlash } from "./removeTrailingSlash.js";
 
 export function kyselyQueriesPath(srcPath: string, name: string): string {
-  return `${removeTrailingSlash(srcPath)}/queries/${snakeCase(name)}`;
+  return `${removeTrailingSlash(srcPath)}/${snakeCase(name)}`;
 }
